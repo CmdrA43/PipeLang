7646e829e640062f2fb5271d9a3ef38bad3f52fa
CmdrA43 <144743060+CmdrA43@users.noreply.github.com>
2026-05-29 13:06:55 -0700

    I do not know how paths work

--- source/main.cpp
+++ source/main.cpp
@@ -204,7 +204,7 @@ class Lexer{
 Lexer lexer;
 
 int main(){
-    std::ifstream file("tests/example1.txt");
+    std::ifstream file("../tests/example1.txt");
     std::stringstream buffer;
     buffer << file.rdbuf();
     std::string content = buffer.str();
