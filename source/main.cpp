eaa9656f851f0cda7bbb924bb6568e53d0c11807
CmdrA43 <capnred43@gmail.com>
2026-06-11 19:53:13 -0700

    created support for file reading as argument

--- source/main.cpp
+++ source/main.cpp
@@ -2,11 +2,35 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <filesystem>
 #include "lexer.h"
 #include "parser.h"
 
-int main(){
-    std::ifstream file("example1.txt");
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[]){
+
+    if(argc < 2){
+        std::cerr << "Error: no input file specified.\n";
+        std::cerr << "Usage: " << argv[0] << " <source_file_path>\n";
+        return 1;
+    };
+
+    fs::path filePath(argv[1]);
+
+    if(!fs::exists(filePath)){
+        std::cerr << "Error: File '" << filePath.string() << "' does not exist.\n";
+        return 1;
+    }
+
+    if (!fs::is_regular_file(filePath)) {
+        std::cerr << "Error: '" << filePath.string() << "' is not a valid file.\n";
+        return 1;
+    }
+
+    std::cout << "Successfully loaded target file: " << fs::absolute(filePath) << "\n";
+
+    std::ifstream file(filePath.string());
     std::stringstream buffer;
     
     buffer << file.rdbuf();
